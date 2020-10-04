8a66d12f956e3ed067543cab61d630b44ecc41eb
Lee Norris <lee.norris@arcadia.io>
2020-10-04 14:55:41 -0400

    fix: endpoint url

--- src/lib/services/s3-publisher.service.ts
+++ src/lib/services/s3-publisher.service.ts
@@ -3,12 +3,14 @@ import { execRx } from '@onivoro/server-process';
 export class S3PublisherService {
     static publish(sourcePath: string, targetPath: string, localStackUrl?: string) {
         const target = `s3://${targetPath}/`.replace('s3://s3://', 's3://');
-        const program = localStackUrl ? `awslocal ${localStackUrl}` : `aws`;
+        const program = localStackUrl ? `awslocal --endpoint-url=${localStackUrl}` : `aws`;
+
         const cmd = [
             `${program} s3 rm ${target} --recursive `,
             `NODE_ENV=production nx build --prod pdf-browser `,
             `${program} s3 cp ${sourcePath} ${target} --acl public-read --recursive`
         ].join(' && ')
+
         return execRx(cmd);
     }
 }
