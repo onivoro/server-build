56a8585804851791b2c995d2d617e6d0f0bbbc5f
Lee Norris <lee.norris@arcadia.io>
2020-10-04 14:46:42 -0400

    chore: s3 publisher

--- src/lib/services/s3-publisher.service.ts
+++ src/lib/services/s3-publisher.service.ts
@@ -5,7 +5,6 @@ export class S3PublisherService {
         const target = `s3://${targetPath}/`.replace('s3://s3://', 's3://');
         const program = localStackUrl ? `awslocal ${localStackUrl}` : `aws`;
         const cmd = [
-            `rm -rf dist`,
             `${program} s3 rm ${target} --recursive `,
             `NODE_ENV=production nx build --prod pdf-browser `,
             `${program} s3 cp ${sourcePath} ${target} --acl public-read --recursive`
