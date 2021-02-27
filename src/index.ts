b1a4a5439d94ca349f43acebdf2e4397e47fb6c0
Lee Norris <lee.norris@arcadia.io>
2021-02-27 12:41:26 -0500

    chore: exports

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,21 @@ export { AngularJsonPath } from './lib/env/angular-json-path';
 export { NxJsonPath } from './lib/env/nx-json-path';
 export { OniJsonPath } from './lib/env/oni-json-path';
 
+export { PackageJsonModel } from './lib/model/package-json.model';
+export { ProjectModel } from './lib/model/project.model';
+
 export { IAngularJson } from './lib/models/i-angular-json';
 export { IAngularProject } from './lib/models/i-angular-project';
 export { INxJson } from './lib/models/i-nx-json';
 export { IOniJson } from './lib/models/i-oni-json';
 
-export { AngularService } from './lib/services/angular.service';
-export { AwsService } from './lib/services/aws.service';
-export { NxService } from './lib/services/nx.service';
-export { OniService } from './lib/services/oni.service';
-export { PublisherService } from './lib/services/publisher.service';
-export { S3Service } from './lib/services/s3.service';
-export { UtilService } from './lib/services/util.service';
+export { AngularService } from './lib/service/angular.service';
+export { AwsService } from './lib/service/aws.service';
+export { NxService } from './lib/service/nx.service';
+export { OniService } from './lib/service/oni.service';
+export { PublisherService } from './lib/service/publisher.service';
+export { S3Service } from './lib/service/s3.service';
+export { UtilService } from './lib/service/util.service';
 
 export { ArchitectBuildTemplatePipe } from './lib/templates/architect-build-template.pipe';
 export { PackageJsonTemplatePipe } from './lib/templates/package-json-template.pipe';
--- src/lib/module/server-build.module.ts
+++ src/lib/module/server-build.module.ts
@@ -1,7 +1,35 @@
 import { Module } from '@nestjs/common';
+import { AngularJsonPath } from '../env/angular-json-path';
+import { NxJsonPath } from '../env/nx-json-path';
+import { OniJsonPath } from '../env/oni-json-path';
 
+import { AngularService } from '../service/angular.service';
+import { AwsService } from '../service/aws.service';
+import { NxService } from '../service/nx.service';
+import { OniService } from '../service/oni.service';
+import { PublisherService } from '../service/publisher.service';
+import { S3Service } from '../service/s3.service';
+import { UtilService } from '../service/util.service';
+
+const paths = [
+    AngularJsonPath,
+    NxJsonPath,
+    OniJsonPath
+];
+
+const providers = [
+    ...paths,
+    AngularService,
+    AwsService,
+    NxService,
+    OniService,
+    PublisherService,
+    S3Service,
+    UtilService,
+];
 @Module({
-    providers: []
+    providers,
+    exports: providers
 })
 export class ServerBuildModule {
 
